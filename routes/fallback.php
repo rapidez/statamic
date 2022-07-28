2fbb41c80a596a0dbfac2fa076dc28b6a828ccea
Bob <bob@justbetter.nl>
2022-07-28 11:14:18 +0200

    Route fallbacks for statamic

---
+++ routes/fallback.php
@@ -0,0 +1,12 @@
+<?php
+use Statamic\Facades\Data;
+
+if ($entry = Data::findByRequestUrl(request()->url())) {
+    if(View::exists($entry->get('template'))) {
+        echo view($entry->get('template'), $entry->data());
+    } else if(View::exists(str($entry->structure()->handle())->singular()->toString())) {
+        echo view(str($entry->structure()->handle())->singular()->toString(), $entry->data());
+    }
+} else {
+    abort(404);
+}

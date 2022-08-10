3000b900dfaf21fdfeb429e9b93d94a7b7a6d24f
Bob Wezelman <bob@justbetter.nl>
2022-08-10 13:18:46 +0200

    Globals support (#4)

--- src/RapidezStatamicServiceProvider.php
+++ src/RapidezStatamicServiceProvider.php
@@ -4,11 +4,39 @@ namespace Rapidez\Statamic;
 
 use Illuminate\Support\ServiceProvider;
 use TorMorten\Eventy\Facades\Eventy;
+use Statamic\Facades\GlobalSet;
+use Illuminate\Support\Facades\View;
+use Statamic\Facades\Site;
 
 class RapidezStatamicServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        $this->bootRoutes()
+        ->bootGlobals();
+    }
+
+    public function bootRoutes() : self
+    {
         Eventy::addFilter('routes', fn ($routes) => array_merge($routes ?: [], [__DIR__.'/../routes/fallback.php']));
+
+        return $this;
+    }
+
+    public function bootGlobals() : self
+    {
+        View::composer('rapidez::layouts.app', function ($view) {
+            foreach (GlobalSet::all() as $set) {
+                foreach ($set->localizations() as $locale => $variables) {
+                    if ($locale == Site::current()->handle()) {
+                        $data[$set->handle()] = $variables;
+                    }
+                }
+            }
+
+            $view->with('globals', (object)$data);
+        });
+
+        return $this;
     }
 }

18ab816efae129a5d5410853028c212648b70c59
Bob Wezelman <bob@justbetter.nl>
2022-10-03 13:15:38 +0200

    Give data as a statamic values object to templates (#5)

--- routes/fallback.php
+++ routes/fallback.php
@@ -1,7 +1,7 @@
 <?php
 use Statamic\Facades\Data;
 
-if ($entry = Data::findByRequestUrl(request()->url())) {
+if ($entry = Statamic\Facades\Entry::findByUri(request()->path())->toResponse(request())) {
     if (View::exists($entry->get('template'))) {
         echo view($entry->get('template'), $entry);
     } else if (View::exists(str($entry->structure()->handle())->singular()->toString())) {

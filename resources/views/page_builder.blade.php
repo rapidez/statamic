1023025f5ccc58800b789cde2b52e2ada17c146f
Roy Duineveld <royduineveld@gmail.com>
2023-05-04 13:44:50 +0200

    Page builder include fallback fix

--- resources/views/page_builder.blade.php
+++ resources/views/page_builder.blade.php
@@ -1,6 +1,9 @@
 @foreach ($content as $set)
-    @includeFirst(['page_builder.' . $set['type'], 'rapidez-statamic::page_builder.' . $set['type']], $set)
-    @if (!View::exists('page_builder.' . $set['type']) && !View::exists('rapidez-statamic::page_builder.' . $set['type']) && !app()->environment('production'))
-        <hr>View for set {{ $set['type'] }} not found.<hr>
+    @if (!View::exists('page_builder.' . $set['type']) && !View::exists('rapidez-statamic::page_builder.' . $set['type']))
+        @if(!app()->environment('production')
+            <hr>View for set {{ $set['type'] }} not found.<hr>
+        @endif
+    @else
+        @includeFirst(['page_builder.' . $set['type'], 'rapidez-statamic::page_builder.' . $set['type']], $set)
     @endif
 @endforeach

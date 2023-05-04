1929cb350db92e356e4f052f39fbffa272a3486f
Roy Duineveld <royduineveld@gmail.com>
2023-05-04 13:45:17 +0200

    Typo

--- resources/views/page_builder.blade.php
+++ resources/views/page_builder.blade.php
@@ -1,6 +1,6 @@
 @foreach ($content as $set)
-    @if (!View::exists('page_builder.' . $set['type']) && !View::exists('rapidez-statamic::page_builder.' . $set['type']))
-        @if(!app()->environment('production')
+    @if(!View::exists('page_builder.' . $set['type']) && !View::exists('rapidez-statamic::page_builder.' . $set['type']))
+        @if(!app()->environment('production'))
             <hr>View for set {{ $set['type'] }} not found.<hr>
         @endif
     @else

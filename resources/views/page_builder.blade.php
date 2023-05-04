f7fcb8885df20be4b2c30cabb7bbb5357c783a9f
Roy Duineveld <royduineveld@gmail.com>
2023-05-04 13:06:43 +0200

    @includeFirst fallback

--- resources/views/page_builder.blade.php
+++ resources/views/page_builder.blade.php
@@ -1,6 +1,6 @@
 @foreach ($content as $set)
-    @includeIf('rapidez-statamic::page_builder.' . $set['type'], $set)
-    @if (!View::exists('rapidez-statamic::page_builder.' . $set['type']) && !app()->environment('production'))
+    @includeFirst(['page_builder.' . $set['type'], 'rapidez-statamic::page_builder.' . $set['type']], $set)
+    @if (!View::exists('page_builder.' . $set['type']) && !View::exists('rapidez-statamic::page_builder.' . $set['type']) && !app()->environment('production'))
         <hr>View for set {{ $set['type'] }} not found.<hr>
     @endif
 @endforeach

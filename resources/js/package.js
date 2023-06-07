b3e2c0e466e0b67cd89049f8e9db21b366fc2ba7
Jade Geels <jade@justbetter.nl>
2023-06-07 13:53:15 +0200

    Remove unnecessary prototype setter

--- resources/js/package.js
+++ resources/js/package.js
@@ -1,7 +1,3 @@
 import 'Vendor/rapidez/core/resources/js/vue'
 
-// Ensure Vue has access to global `Statamic` component
-Vue.prototype.Statamic = window.Statamic;
-
-// Create `<form-conditions>` component to be used with Vue JS driver
 Vue.component('form-conditions', () => import('./components/FormConditions.vue'));

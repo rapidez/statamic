6ff4f29ea45f329b33be1368837bddc837df1fcf
indykoning <15870933+indykoning@users.noreply.github.com>
2023-07-04 10:30:50 +0200

    Use url helpers (#37)

--- resources/views/breadcrumbs.blade.php
+++ resources/views/breadcrumbs.blade.php
@@ -1,6 +1,6 @@
 <x-rapidez::breadcrumbs>
     @foreach(Statamic::tag('nav:breadcrumbs')->param('include_home', false) as $item)
-        <x-rapidez::breadcrumb :active="$item->is_current" :url="$item->url" :position="$loop->iteration + 1">
+        <x-rapidez::breadcrumb :active="$item->is_current" :url="url($item->url)" :position="$loop->iteration + 1">
             {{ $item->title }}
         </x-rapidez::breadcrumb>
     @endforeach

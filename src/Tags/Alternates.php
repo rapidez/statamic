f1d331d185e27ea477508fc10d9a2d1b05629d5f
Roy Duineveld <royduineveld@gmail.com>
2023-10-25 17:04:23 +0200

    Only use the base site url for the homepage

--- src/Tags/Alternates.php
+++ src/Tags/Alternates.php
@@ -10,18 +10,19 @@ class Alternates extends Tags
     public function index()
     {
         $page = $this->params->get('page');
+        $isHome = request()->path() == '/';
 
         return collect(Config::getOtherLocales($page->locale))
-            ->filter(fn ($locale) => $page->isRoot() || $page->in($locale)?->status() === 'published')
+            ->filter(fn ($locale) => $isHome || $page->in($locale)?->status() === 'published')
             ->filter(function ($locale) use ($page) {
                 $currentPageSiteGroup = Config::getSite($page->locale)->attributes()['group'] ?? false;
                 $otherLocaleSiteGroup = Config::getSite($locale)->attributes()['group'] ?? false;
 
                 return $currentPageSiteGroup === $otherLocaleSiteGroup;
             })
-            ->mapWithKeys(function ($site) use ($page) {
+            ->mapWithKeys(function ($site) use ($page, $isHome) {
                 $lang = str(Config::getSite($site)->locale())->replace('_', '-')->lower()->value();
-                $url = $page->isRoot()
+                $url = $isHome
                     ? Config::getSite($site)->absoluteUrl()
                     : $page->in($site)->absoluteUrl();
 

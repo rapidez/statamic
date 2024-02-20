8cbfa751783b00212e4d83d8b31c424ff64c627b
Roy Duineveld <royduineveld@gmail.com>
2024-02-20 17:57:22 +0100

    Make sure the page collection has the site configured (#55)

--- src/Tags/Alternates.php
+++ src/Tags/Alternates.php
@@ -13,7 +13,7 @@ class Alternates extends Tags
         $isHome = request()->path() == '/';
 
         return collect(Config::getOtherLocales($page->locale))
-            ->filter(fn ($locale) => $isHome || $page->in($locale)?->status() === 'published')
+            ->filter(fn ($locale) => $isHome || ($page->collection->sites()->contains($locale) && $page->in($locale)?->status() === 'published'))
             ->filter(function ($locale) use ($page) {
                 $currentPageSiteGroup = Config::getSite($page->locale)->attributes()['group'] ?? false;
                 $otherLocaleSiteGroup = Config::getSite($locale)->attributes()['group'] ?? false;

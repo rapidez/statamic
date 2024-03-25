07215e607d33cca0ae444fbbe565b52aaac514a2
Bob <bob@justbetter.nl>
2024-03-25 10:10:37 +0100

    Feedback

--- resources/views/page_builder/brand_overview.blade.php
+++ resources/views/page_builder/brand_overview.blade.php
@@ -1,14 +1,15 @@
 @php
     $brands = \Statamic\Facades\Entry::query()
         ->where('collection', 'brands')
+        ->where('site', \Statamic\Facades\Site::current()->handle())
         ->orderBy('title')
         ->get();
 
-    $grouped = $brands->groupBy(function(\Statamic\Eloquent\Entries\Entry $item,$key) {
+    $grouped = $brands->groupBy(function(\Statamic\Eloquent\Entries\Entry $item, int $key) {
         return $item->title[0];
     });
-
 @endphp
+
 <div class="lg:container">
     @if($include_brand_list)
         <div>

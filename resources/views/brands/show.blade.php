cf188c4506c76d43164eaf9a8bb319d24b127ca2
Bob Wezelman <bob@justbetter.nl>
2024-03-25 16:23:57 +0100

    Brand pages and overview (#59)

---
+++ resources/views/brands/show.blade.php
@@ -0,0 +1,27 @@
+@extends('rapidez::layouts.app')
+
+@section('title', $meta_title->value() ?: $title)
+@section('description', $meta_description)
+@push('head')
+    @foreach(Statamic::tag('alternates')->params(compact('page')) as $lang => $url)
+        <link rel="alternate" hreflang="{{ $lang }}" href="{{ $url }}" />
+    @endforeach
+@endpush
+
+@php($brandAttribute = \Rapidez\Core\Models\Attribute::find(config('rapidez.statamic.runway.brand_attribute_id')))
+
+@section('content')
+    @includeWhen(!$is_homepage, 'rapidez-statamic::breadcrumbs')
+
+    @if ($title)
+        <h1 class="font-bold text-3xl mb-5">{{ $title }}</h1>
+    @endif
+    @if ($brand_content)
+        <div class="prose prose-green max-w-none">
+            @include('rapidez-statamic::page_builder.content', ['content' => $brand_content])
+        </div>
+    @endif
+    <x-rapidez::listing query="{ terms: { '{{ $brandAttribute->code }}.keyword': ['{{ $title }}'] } }"/>
+
+    @include('rapidez-statamic::page_builder')
+@endsection
---
+++ resources/views/page_builder/brandlist.blade.php
@@ -0,0 +1,46 @@
+@php
+    $brands = \Statamic\Facades\Entry::query()
+        ->where('collection', 'brands')
+        ->where('site', \Statamic\Facades\Site::current()->handle())
+        ->orderBy('title')
+        ->get();
+
+    $grouped = $brands->groupBy(function(\Statamic\Eloquent\Entries\Entry $item, int $key) {
+        return $item->title[0];
+    });
+@endphp
+
+<div class="lg:container">
+    <div class="flex flex-col">
+        @if($include_table_of_contents)
+            <ul class="flex">
+                @foreach($grouped->keys() as $letter)
+                    <li><a href="#{{ $letter }}">{{ $letter }}</a></li>
+                @endforeach
+            </ul>
+        @endif
+
+        <div class="flex flex-col">
+            @foreach($grouped as $letter => $group)
+                <div class="flex w-full">
+                    <span id="{{ $letter }}" class="text-xl">
+                        {{ $letter }}
+                    </span>
+                    <ul class="flex row-wrap">
+                        @foreach($group as $brand)
+                            <li>
+                                <a href="{{ $brand->url() }}">
+                                    @if(!$brand->image->isEmpty())
+                                        @responsive($brand->image->first())
+                                    @else
+                                        {{ $brand->title }}
+                                    @endif
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</div>

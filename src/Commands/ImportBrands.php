58cba4f69be4067a183acb2617dd90f9821c8d8c
Bob <bob@justbetter.nl>
2024-03-29 14:50:04 +0100

    Remove empty lines

--- src/Commands/ImportBrands.php
+++ src/Commands/ImportBrands.php
@@ -39,18 +39,16 @@ class ImportBrands extends Command
 
                 if (Schema::hasTable('amasty_amshopby_option_setting')) {
                     $amastyData = DB::table('amasty_amshopby_option_setting')->where('value', '=', $brand->option_id)->first();
-
                     $extraData['meta_title'] = $amastyData->meta_title;
                     $extraData['meta_description'] = $amastyData->meta_description;
 
                     if ($amastyData->image) {
                         $file = file_get_contents(config('rapidez.media_url').'/amasty/shopby/option_images/'.$amastyData->image, false, stream_context_create(['ssl' => ['verify_peer' => false, 'verify_peer_name' => false]]));
                         Storage::disk('assets')->put('brands/' . $amastyData->image, $file);
-
                         $extraData['image'] = 'brands/' . $amastyData->image;
                     }
                 }
-                
+
                 $statamicEntryAction::createEntry(
                     [
                         'collection' => config('rapidez.statamic.import.brands.collection', 'brands'),

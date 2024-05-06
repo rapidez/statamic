d0d89a746632ead34faaa891658ec898a64934c4
Roene Verbeek <85165259+Roene-JustBetter@users.noreply.github.com>
2024-05-06 16:48:55 +0200

    Added container classes on places where needed

--- resources/views/brands/show.blade.php
+++ resources/views/brands/show.blade.php
@@ -11,17 +11,23 @@
 @php($brandAttribute = \Rapidez\Core\Models\Attribute::find(config('rapidez.statamic.runway.brand_attribute_id')))
 
 @section('content')
-    @includeWhen(!$is_homepage, 'rapidez-statamic::breadcrumbs')
-
-    @if ($title)
-        <h1 class="font-bold text-3xl mb-5">{{ $title }}</h1>
-    @endif
-    @if ($brand_content)
-        <div class="prose prose-green max-w-none">
-            @include('rapidez-statamic::page_builder.content', ['content' => $brand_content])
+    <div class="lg:container lg:mt-4 lg:mb-20">
+        <div class="max-lg:container">
+            @includeWhen(!$is_homepage, 'rapidez-statamic::breadcrumbs')
         </div>
-    @endif
-    <x-rapidez::listing query="{ terms: { '{{ $brandAttribute->code }}.keyword': ['{{ $title }}'] } }"/>
 
-    @include('rapidez-statamic::page_builder')
+        @if ($title)
+            <h1 class="font-bold text-3xl mb-5">{{ $title }}</h1>
+        @endif
+        @if ($brand_content)
+            <div class="prose prose-green max-w-none">
+                @include('rapidez-statamic::page_builder.content', ['content' => $brand_content])
+            </div>
+        @endif
+        <x-rapidez::listing query="{ terms: { '{{ $brandAttribute->code }}.keyword': ['{{ $title }}'] } }"/>
+
+        <div class="mb-8 max-lg:container">
+            @include('rapidez-statamic::page_builder')
+        </div>
+    </div>
 @endsection

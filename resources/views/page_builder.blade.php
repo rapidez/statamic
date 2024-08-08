0fa76edd4afe055daa8e7c9c3ba6fa528f77105d
Kevin Meijer <kev10@live.nl>
2024-08-08 09:18:17 +0200

    Statamic 5 support + install command (#67)

--- resources/views/page_builder.blade.php
+++ resources/views/page_builder.blade.php
@@ -1,3 +1,3 @@
 @foreach ($content as $set)
-    @includeFirstSafe(['page_builder.' . $set['type'], 'rapidez-statamic::page_builder.' . $set['type']], $set)
+    @includeFirstSafe(['page_builder.' . $set['type'], 'rapidez-statamic::page_builder.' . $set['type']], optionalDeep($set)->toArray())
 @endforeach

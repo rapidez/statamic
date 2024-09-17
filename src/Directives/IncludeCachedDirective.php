2060964120ff33011d2f846f1917ba831480dfb2
Bob <bob@justbetter.nl>
2024-09-17 13:54:33 +0200

    Static directive

--- src/Directives/IncludeCachedDirective.php
+++ src/Directives/IncludeCachedDirective.php
@@ -2,16 +2,37 @@
 
 namespace Rapidez\Statamic\Directives;
 
+use Statamic\StaticCaching\Cachers\Writer;
 use Statamic\View\View;
 
 class IncludeCachedDirective
 {
-    public static function handle(string $view, array $vars = []): View
+    public static function handle(string $view, array $vars = [])
     {
-        return (new View)
-            ->template($view)
-            ->with($vars);
+        $namespace = str($view)->explode('::');
+        $path = '';
+    
+        if ($namespace->count() > 1) {
+            $path = $namespace->first() . '/';
+        }
 
+        $path .= str($namespace->last())->replace('.', '/');
+
+        $path = config('statamic.static_caching.strategies.full.path') . '/' . $path . '.html';
+
+        if (!file_exists($path)) {
+            $view = (new View)
+                ->template($view)
+                ->with($vars)
+                ->render();
+            
+            $writer = app(Writer::class);
+            $writer->write($path, $view);
+            
+            return $view;
+        }
+
+        return readfile($path);
     }
 
     public static function bind(): void

928478c7004e2c42ea6b5a1d2d16de9873b5b570
indykoning <15870933+indykoning@users.noreply.github.com>
2024-09-30 15:43:03 +0200

    Add once around getTable functions

--- src/Models/Product.php
+++ src/Models/Product.php
@@ -24,6 +24,6 @@ class Product extends Model
 
     public function getTable()
     {
-        return 'catalog_product_flat_' . (Statamic::isCpRoute() ? (Site::selected()->attributes['magento_store_id'] ?? '1') : Site::current()->attributes['magento_store_id'] ?? '1');
+        return 'catalog_product_flat_' . once(fn() => (Statamic::isCpRoute() ? (Site::selected()->attributes['magento_store_id'] ?? '1') : Site::current()->attributes['magento_store_id'] ?? '1'));
     }
 }

0c722de0a73407bf3f1b8051122cd11b73d63d8d
Roy Duineveld <royduineveld@gmail.com>
2024-10-25 16:00:01 +0200

    Use the Statamic web middleware

--- src/Http/Controllers/StatamicRewriteController.php
+++ src/Http/Controllers/StatamicRewriteController.php
@@ -3,15 +3,26 @@
 namespace Rapidez\Statamic\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\View;
 use Statamic\Exceptions\NotFoundHttpException;
 use Statamic\Facades\Entry;
 use Statamic\Http\Controllers\FrontendController;
 
-class StatamicRewriteController
+class StatamicRewriteController extends Controller
 {
+    protected Controller $controller;
+
+    public function __construct()
+    {
+        $this->middleware('statamic.web');
+
+        $this->controller = new FrontendController;
+    }
+
     public function __invoke(Request $request)
     {
-        return (new FrontendController)->index($request);
+        return $this->controller->index($request);
     }
 }

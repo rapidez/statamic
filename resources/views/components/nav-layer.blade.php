8b136e0d5e63de5616275a5dd3a426661f555a52
Roy Duineveld <royduineveld@gmail.com>
2024-11-06 15:27:23 +0100

    Markdown description

--- resources/views/components/nav-layer.blade.php
+++ resources/views/components/nav-layer.blade.php
@@ -1,20 +1,19 @@
 {{--
-    This is a mobile navigation slideover component for handling multi-level navgiations It makes use of recursion to handle multiple levels of navgation dynamically.
+This is a mobile navigation slideover component for handling multi-level navigations. It makes use of recursion to handle multiple levels of navigation dynamically.
 
-    Properties:
-        id:             Unique identifier for the current navigation layer.
-        title:          Title displayed in the header. Defaults to "Menu".
-        hasParent:      Indicates if the current menu is a child of another menu. Defaults to false.
-        tag:            Base HTML tag for the slideover component. Set this to 'form' for the main navigation or 'div' for nested menus.
-        parentUrl:      URL used for navigating to the parent item when in a nested menu. Displayed with a "Go to" link.
+## Properties
+- `id`           Unique identifier for the current navigation layer.
+- `children`     Navigation children data
+- `title`        Title displayed in the header. Defaults to "Menu".
+- `hasParent`    Indicates if the current menu is a child of another menu. Defaults to false.
+- `tag`          Base HTML tag for the slideover component. Set this to 'form' for the main navigation or 'div' for nested menus.
+- `parentUrl`    URL used for navigating to the parent item when in a nested menu. Displayed with a "Go to" link.
 
-    Slots:
-        headerbutton:   This slot allows the customization of the button placed at the left in the header for navigation.
-
-    The component uses recursion to handle submenus. When a menu item has children, it creates another instance of this slideover component for the nested navigation layer.
+## Slots
+- `headerButton` This slot allows the customization of the button placed at the left in the header for navigation.
 --}}
 @props(['id', 'children', 'title' => __('Menu'), 'hasParent' => false, 'tag' => 'form', 'parentUrl' => ''])
-@slots(['headerbutton'])
+@slots(['headerButton'])
 
 <x-rapidez::slideover.mobile
     :title="(string) $title"
@@ -22,11 +21,11 @@
     :$hasParent
     :$tag
 >
-    <x-slot:headerbutton>
+    <x-slot:headerButton>
         <div class="absolute left-0 top-1/2 -translate-y-1/2 cursor-pointer text-white">
             @include('rapidez-statamic::navigation.header-button')
         </div>
-    </x-slot:headerbutton>
+    </x-slot:headerButton>
     <div class="bg-inactive-100 flex w-full flex-1 flex-col">
         <ul class="mt-5 flex flex-col divide-y border-y bg-white">
             @if ($hasParent && $parentUrl)

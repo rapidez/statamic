102e3b4df364fce61e7bf847ba3e7ef5eb0ee050
Kevin Meijer <kevin@justbetter.nl>
2024-12-13 13:11:00 +0100

    Simplify check

--- src/RapidezStatamicServiceProvider.php
+++ src/RapidezStatamicServiceProvider.php
@@ -248,9 +248,7 @@ class RapidezStatamicServiceProvider extends ServiceProvider
     public function getSiteHandleByStoreId(): string
     {
         $site = Site::all()
-            ->filter(fn($_site) => 
-                ($_site?->attributes()['magento_store_id'] ?? false) &&
-                ((int) $_site?->attributes()['magento_store_id'] === (int) config('rapidez.store')))
+            ->filter(fn($_site) => ($_site?->attributes()['magento_store_id'] ?? null) == config('rapidez.store'))
             ->first();
 
         return $site?->handle() ?? config('rapidez.store_code');

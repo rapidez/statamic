80837aab2b78845c07153b21335901d93e94dd13
Bob <bob@justbetter.nl>
2025-01-02 14:33:06 +0100

    Set the csrf token on vue when it get's reloaded by statamic

--- resources/js/package.js
+++ resources/js/package.js
@@ -1,3 +1,7 @@
 import 'Vendor/rapidez/core/resources/js/vue'
 
 Vue.component('form-conditions', () => import('./components/FormConditions.vue'));
+
+document.addEventListener('statamic:nocache.replaced', (e) => {
+    window.app.csrfToken = e?.detail?.csrf ?? window.app.csrfToken
+})

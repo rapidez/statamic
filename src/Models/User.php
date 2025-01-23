04d6eb1749b9234067d06426681689b9d79e6e38
Kevin Meijer <kevin@justbetter.nl>
2025-01-23 10:53:26 +0100

    Refactored install command

---
+++ src/Models/User.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Rapidez\Statamic\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Statamic\Notifications\PasswordReset;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+    public function sendPasswordResetNotification($token): void
+    {
+        $this->notify(new PasswordReset($token));
+    }
+}

ef56846229307fb470abd4f117edb6ad77b9fcf4
Kevin Meijer <kevin@justbetter.nl>
2025-01-23 11:17:59 +0100

    Removed old code

--- src/Models/User.php
+++ src/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace Rapidez\Statamic\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;

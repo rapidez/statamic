2fe11cc470b8965f73073dcbf795a215afd3ab1a
Roene <roene@justbetter.nl>
2025-07-28 11:50:58 +0200

    Add correct labels for form fields

--- resources/views/form_fields/text.blade.php
+++ resources/views/form_fields/text.blade.php
@@ -1,14 +1,16 @@
-<x-rapidez::input
-    :name="$field['handle']"
-    :type="$field['input_type']"
-    :label="$field['display']"
-    :value="$field['old'] ?: $field['value']"
-    :placeholder="$field['placeholder'] ?? false"
-    :class="$field['error'] ? 'border-red-500' : ''"
-    :maxlength="$field['character_limit'] ?? false"
-    :required="in_array('required', $field['validate'] ?? [])"
-    v-model="formData.{{ $field['handle'] }}"
-/>
-@if($field['error'])
-    <div class="text-red-500">{{ $field['error'] }}</div>
-@endif
+<label>
+    <x-rapidez::label>{{ $field['display'] }}</x-rapidez::label>
+    <x-rapidez::input
+        :name="$field['handle']"
+        :type="$field['input_type']"
+        :value="$field['old'] ?: $field['value']"
+        :placeholder="$field['placeholder'] ?? false"
+        :class="$field['error'] ? 'border-red-500' : ''"
+        :maxlength="$field['character_limit'] ?? false"
+        :required="in_array('required', $field['validate'] ?? [])"
+        v-model="formData.{{ $field['handle'] }}"
+    />
+    @if($field['error'])
+        <div class="text-red-500">{{ $field['error'] }}</div>
+    @endif
+</label>

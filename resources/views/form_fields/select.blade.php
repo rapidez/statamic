ecabea5e208dc21be05cdc7e5e6ebff95d5878e2
Roene Verbeek <85165259+Roene-JustBetter@users.noreply.github.com>
2025-07-29 09:45:16 +0200

    Use new form labels (#151)

--- resources/views/form_fields/select.blade.php
+++ resources/views/form_fields/select.blade.php
@@ -1,19 +1,21 @@
-<x-rapidez::select
-    :name="$field['handle']"
-    :label="$field['display']"
-    :value="$field['old'] ?: $field['default'] ?? ''"
-    :class="$field['error'] ? 'border-red-500' : ''"
-    :required="in_array('required', $field['validate'] ?? [])"
-    v-model="formData.{{ $field['handle'] }}"
->
-    @if (!($field['default'] ?? ''))
-        <option value="null" disabled selected>
-            {{ $field['placeholder'] ?? '' }}
-        </option>
-    @endif
-    @foreach ($field['options'] ?? [] as $option => $label)
-        <option value="{{ $option }}" @selected($option == $field['default'] ?? '')>
-            {{ $label }}
-        </option>
-    @endforeach
-</x-rapidez::select>
+<label>
+    <x-rapidez::label>{{ $field['display'] }}</x-rapidez::label>
+    <x-rapidez::select
+        :name="$field['handle']"
+        :value="$field['old'] ?: $field['default'] ?? ''"
+        :class="$field['error'] ? 'border-red-500' : ''"
+        :required="in_array('required', $field['validate'] ?? [])"
+        v-model="formData.{{ $field['handle'] }}"
+    >
+        @if (!($field['default'] ?? ''))
+            <option value="null" disabled selected>
+                {{ $field['placeholder'] ?? '' }}
+            </option>
+        @endif
+        @foreach ($field['options'] ?? [] as $option => $label)
+            <option value="{{ $option }}" @selected($option == $field['default'] ?? '')>
+                {{ $label }}
+            </option>
+        @endforeach
+    </x-rapidez::select>
+</label>
--- resources/views/form_fields/textarea.blade.php
+++ resources/views/form_fields/textarea.blade.php
@@ -1,13 +1,15 @@
-<x-rapidez::textarea
-    :name="$field['handle']"
-    :label="$field['display']"
-    :value="$field['value']"
-    :placeholder="$field['placeholder'] ?? false"
-    :class="$field['error'] ? 'border-red-500' : ''"
-    :maxlength="$field['character_limit'] ?? false"
-    :required="in_array('required', $field['validate'] ?? [])"
-    v-model="formData.{{ $field['handle'] }}"
-/>
-@if($field['error'])
-    <div class="text-red-500">{{ $field['error'] }}</div>
-@endif
+<label>
+    <x-rapidez::label>{{ $field['display'] }}</x-rapidez::label>
+    <x-rapidez::textarea
+        :name="$field['handle']"
+        :value="$field['value']"
+        :placeholder="$field['placeholder'] ?? false"
+        :class="$field['error'] ? 'border-red-500' : ''"
+        :maxlength="$field['character_limit'] ?? false"
+        :required="in_array('required', $field['validate'] ?? [])"
+        v-model="formData.{{ $field['handle'] }}"
+    />
+    @if($field['error'])
+        <div class="text-red-500">{{ $field['error'] }}</div>
+    @endif
+</label>

5971cea35fc249767a2c59a1dac3716471370df0
Jade Geels <jade@justbetter.nl>
2025-08-21 13:47:02 +0200

    Create dictionary form field type

---
+++ resources/views/form_fields/dictionary.blade.php
@@ -0,0 +1,21 @@
+<label>
+    <x-rapidez::label>{{ $field['display'] }}</x-rapidez::label>
+    <x-rapidez::input.select
+        :name="$field['handle']"
+        :value="$field['old'] ?: $field['default'] ?? ''"
+        :class="$field['error'] ? 'border-red-500' : ''"
+        :required="in_array('required', $field['validate'] ?? [])"
+        v-model="formData.{{ $field['handle'] }}"
+    >
+        @if (!($field['default'] ?? ''))
+            <option value="null" disabled selected>
+                {{ $field['placeholder'] ?? '' }}
+            </option>
+        @endif
+        @foreach ($field['options'] ?? [] as $option => $label)
+            <option value="{{ $option }}" @selected($option == $field['default'] ?? '')>
+                {{ $label }}
+            </option>
+        @endforeach
+    </x-rapidez::input.select>
+</label>

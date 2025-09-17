ff9d20cd446caaf5240e833ca60e6c295ae2df8c
Jade Geels <jade@justbetter.nl>
2025-09-17 11:50:32 +0200

    Brand blueprint fix (#162)

--- src/RapidezStatamicServiceProvider.php
+++ src/RapidezStatamicServiceProvider.php
@@ -178,6 +178,7 @@ class RapidezStatamicServiceProvider extends ServiceProvider
         }
 
         if (! Statamic::isCpRoute()) {
+            Blueprint::addNamespace('collections.brands', resource_path('blueprints/vendor/runway'));
             Blueprint::addNamespace('collections.products', resource_path('blueprints/vendor/runway'));
             Blueprint::addNamespace('collections.categories', resource_path('blueprints/vendor/runway'));
         }

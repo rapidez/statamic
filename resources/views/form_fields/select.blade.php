acad0de59606094bfe36779f5f59cbd76836edaf
Jade Geels <jade@justbetter.nl>
2025-09-30 10:28:40 +0200

    Add translations to labels in forms (#167)

--- resources/views/form_fields/select.blade.php
+++ resources/views/form_fields/select.blade.php
@@ -1,5 +1,5 @@
 <label>
-    <x-rapidez::label>{{ $field['display'] }}</x-rapidez::label>
+    <x-rapidez::label>@lang($field['display'])</x-rapidez::label>
     <x-rapidez::input.select
         :name="$field['handle']"
         :value="$field['old'] ?: $field['default'] ?? ''"

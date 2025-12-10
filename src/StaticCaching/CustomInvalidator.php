58a07aecaf3aff243d59e390bd0f4377b861f7fa
Jade Geels <jade@justbetter.nl>
2025-12-10 09:58:49 +0100

    Also invalidate on NavTree changes (#177)

--- src/StaticCaching/CustomInvalidator.php
+++ src/StaticCaching/CustomInvalidator.php
@@ -4,6 +4,7 @@ namespace Rapidez\Statamic\StaticCaching;
 
 use Statamic\Eloquent\Globals\GlobalSet;
 use Statamic\Eloquent\Structures\Nav;
+use Statamic\Eloquent\Structures\NavTree;
 use Statamic\Entries\Entry;
 use Statamic\StaticCaching\DefaultInvalidator;
 use Statamic\Support\Str;
@@ -12,7 +13,12 @@ class CustomInvalidator extends DefaultInvalidator
 {
     public function invalidate($item)
     {
-        if ($item instanceof GlobalSet || $item instanceof Nav || $this->rules === 'all') {
+        if (
+            $item instanceof GlobalSet
+            || $item instanceof Nav
+            || $item instanceof NavTree
+            || $this->rules === 'all'
+        ) {
             return $this->cacher->flush();
         }
 

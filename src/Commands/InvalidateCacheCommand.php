0426e1cf5af73d4abd5ae5aeba07d5d8b49efd49
Jade Geels <jade@justbetter.nl>
2026-01-21 11:28:46 +0100

    Minimize memory usage for retrieving product rewrite URLs

--- src/Commands/InvalidateCacheCommand.php
+++ src/Commands/InvalidateCacheCommand.php
@@ -75,20 +75,22 @@ class InvalidateCacheCommand extends Command
 
     protected function addProductsUrls(): self
     {
-        $products = config('rapidez.models.product')::withoutGlobalScopes()
+        $productIds = config('rapidez.models.product')::withoutGlobalScopes()
             ->where('updated_at', '>=', $this->latestCheck)
             ->orWhereIn('entity_id', $this->getUpdatedStockProducts())
-            ->with(['parent:entity_id' => ['rewrites']])
-            ->with('rewrites')
-            ->get('entity_id');
+            ->pluck('entity_id');
 
-        foreach ($products as $product) {
-            $this->addUrls($this->getUrlsFromRewrites($product->rewrites));
+        $parentIds = config('rapidez.models.product_link')::whereIn('product_id', $productIds)
+            ->groupBy('parent_id')
+            ->pluck('parent_id');
 
-            if ($product->parent) {
-                $this->addUrls($this->getUrlsFromRewrites($product->parent->rewrites));
-            }
-        }
+        $allIds = $productIds->merge($parentIds)->unique();
+
+        $rewrites = config('rapidez.models.rewrite')::whereIn('entity_id', $allIds)
+            ->where('entity_type', 'product')
+            ->pluck('request_path');
+
+        $this->addUrls($rewrites);
 
         return $this;
     }

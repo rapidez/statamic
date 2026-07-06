33cf5be0ff88613f6594c29e6e23829a2d1d48a0
Jade Geels <jade@justbetter.nl>
2026-07-06 11:43:12 +0200

    Support non-integer key types

--- src/Models/BaseEntry.php
+++ src/Models/BaseEntry.php
@@ -14,6 +14,7 @@ class BaseEntry extends Model
     use Searchable;
 
     protected $table = 'statamic_entries';
+    protected $keyType = 'string';
     protected static $collection;
 
     protected $casts = [
